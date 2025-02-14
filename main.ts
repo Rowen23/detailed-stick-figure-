e3f1d297c8fef93f1df61505062f3f4559291222
Rowen23 <28rrogers@aos92.org>
2025-02-14 10:09:28 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,28 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite2 = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Player)
+})
+let mySprite2: Sprite = null
+let mySprite = sprites.create(assets.image`stick figure more detailed`, SpriteKind.Player)
+animation.runImageAnimation(
+mySprite,
+assets.animation`Punching`,
+500,
+false
+)
